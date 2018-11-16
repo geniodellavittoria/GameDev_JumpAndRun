bc896b33b0f39560a7cf8a5941b0fff1890083bf
Pascal Keusch <pascal.keusch@stud.hslu.ch>
2018-11-16 16:22:22 +0100

    Add ScriptableCharacter

--- Assets/GameObjects/Character/TowerEnemy.cs
+++ Assets/GameObjects/Character/TowerEnemy.cs
@@ -1,10 +1,50 @@
 ﻿using System;
+using UnityEngine;
+
 namespace Application
 {
-    public class NewClass
+    public class ScriptableCharacter : ScriptableObject, ICharacter
     {
-        public NewClass()
+        public ScriptableCharacter()
         {
+        }
+
+
+        public double Height { get; set; }
+
+        public double Speed { get; set; }
+
+        public double Life { get; set; }
+
+        public double Strength { get; set; }
+
+        public bool IsEnemy { get; set; }
+
+        public bool IsDead { get; set; }
+
+        public void attack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void die()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void jump()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void move()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void takeDamage()
+        {
+            throw new NotImplementedException();
         }
     }
 }
